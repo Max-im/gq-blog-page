88aaa7eadbf81e1866d49b30fe847946524b8766
max-im <pogidaevmo@gmail.com>
2022-06-23 19:40:04 +0300

    update post ui

--- client/src/components/CommentsCreate.js
+++ client/src/components/CommentsCreate.js
@@ -2,23 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { useMutation } from '@apollo/client';
 import { CREATE_COMMENT } from '../mutations/comments';
 import { GET_POST } from '../query/posts';
+import { Alert, Button, TextField } from '@mui/material';
 
-export default function CommentsCreate({postId}) {
+export default function CommentsCreate({ postId }) {
   const tmpl = { post: postId, text: '' };
-  const [commentInput, setInput] = useState({...tmpl});
-  const [createComment, { data, loading }] = useMutation(CREATE_COMMENT);
+  const [commentInput, setInput] = useState({ ...tmpl });
+  const [createComment, { data, loading, error }] = useMutation(CREATE_COMMENT, {
+    errorPolicy: 'all',
+  });
 
   const onCreatePost = (e) => {
     e.preventDefault();
     createComment({
       variables: { input: { ...commentInput } },
-      refetchQueries: [{ query: GET_POST, variables: {id: postId} }] 
+      refetchQueries: [{ query: GET_POST, variables: { id: postId } }],
     });
   };
 
   useEffect(() => {
     if (!loading && data) {
-      setInput({...tmpl});
+      setInput({ ...tmpl });
     }
   }, [data]);
 
@@ -28,14 +31,27 @@ export default function CommentsCreate({postId}) {
 
   return (
     <div>
-      <h3>Create Comment</h3>
       <form onSubmit={onCreatePost}>
-        <div style={{marginTop: '10px'}}>
-          <p>Comment:</p>
-          <textarea style={{width: '500px', height: '150px'}} type="text" name="text" value={commentInput.text} onChange={inputChange} />
+        <div style={{ marginTop: '10px' }}>
+          <TextField
+            label="Comment"
+            multiline
+            rows={6}
+            variant="outlined"
+            name="text"
+            value={commentInput.text}
+            onChange={inputChange}
+          />
         </div>
-
-        <button type="submit" style={{marginTop: '10px'}}>Create</button>
+        {error &&
+          error.graphQLErrors.map(({ message }, i) => (
+            <Alert severity="error" key={i}>
+              {message}
+            </Alert>
+          ))}
+        <Button type="submit" sx={{ mt: 2 }} variant="contained">
+          Add Comment
+        </Button>
       </form>
     </div>
   );
--- client/src/pages/PostsItem.js
+++ client/src/pages/PostsItem.js
@@ -6,13 +6,14 @@ import { Link, useParams } from 'react-router-dom';
 import Modal from '../components/UI/Modal/Modal';
 import PostsUpdate from '../components/PostsUpdate';
 import CommentsCreate from '../components/CommentsCreate';
+import { Button, List, ListItem, ListItemText, Typography } from '@mui/material';
 
 export default function PostsItem() {
   const [post, setPost] = useState(null);
   const [visible, setVisible] = useState(false);
   const { user } = useContext(AuthContext);
   const { id } = useParams();
-  const { data, loading, error } = useQuery(GET_POST, {variables: { id }});
+  const { data, loading, error } = useQuery(GET_POST, { variables: { id } });
 
   useEffect(() => {
     if (!loading) {
@@ -23,22 +24,46 @@ export default function PostsItem() {
   return (
     <div>
       {error && error.message}
-      {!(error || loading ) && post && (
+      {!(error || loading) && post && (
         <>
-          {user && user.user.id === post.author.id && 
-            <button onClick={() => setVisible(true)}>Update Post</button>
-          }
-          {visible && <Modal visible={visible} setVisible={setVisible}>
-              <PostsUpdate title={post.title} body={post.body} id={post.id} setVisible={setVisible}/>
-            </Modal>}
-          <h1>{post.title}</h1>
-          <p>{post.body}</p>
-          <p>by {' '} <Link to={`/users/${post.author.id}`}>{post.author.name}</Link></p>
-          <h3>Comments:</h3>
-          <ul>
-            {post.comments.map(c => <li key={c.id}>{c.text}</li>)}
-          </ul>
-          { user && <CommentsCreate postId={post.id}/>}
+          {user && user.user.id === post.author.id && (
+            <>
+              <Button onClick={() => setVisible(true)} variant="contained" sx={{ mt: 3 }}>
+                Update Post
+              </Button>
+              {visible && (
+                <Modal visible={visible} setVisible={setVisible}>
+                  <PostsUpdate
+                    title={post.title}
+                    body={post.body}
+                    id={post.id}
+                    setVisible={setVisible}
+                  />
+                </Modal>
+              )}
+            </>
+          )}
+
+          <Typography variant="h2" component="h2">
+            {post.title}
+          </Typography>
+          <Typography variant="p" component="p">
+            {post.body}
+          </Typography>
+          <p>
+            author: <Link to={`/users/${post.author.id}`}>{post.author.name}</Link>
+          </p>
+          <Typography variant="h4" component="h4">
+            Comments
+          </Typography>
+          {user && <CommentsCreate postId={post.id} />}
+          <List>
+            {post.comments.map(({ id, text }) => (
+              <ListItem key={id}>
+                <ListItemText secondary={text} />
+              </ListItem>
+            ))}
+          </List>
         </>
       )}
     </div>
