1725fc2fca17a81b00c4981c09131f99a6eecbd3
max-im <pogidaevmo@gmail.com>
2022-06-23 18:38:07 +0300

    style uasrs list

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_USERS } from '../query/users';
 import { Link } from 'react-router-dom';
+import { List, ListItem, Skeleton, Typography } from '@mui/material';
 
 export default function Users() {
   const [users, setUsers] = useState([]);
@@ -15,15 +16,19 @@ export default function Users() {
 
   return (
     <div>
-      {loading && 'Loading...'}
-      <h1>Authors:</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <Link to={`/users/${user.id}`}>{user.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <Typography variant="h2" component="h2">
+        Authors
+      </Typography>
+      {loading && [1,2,3,4,5,6,7,8,9,10].map(a => <Skeleton/>)}
+      {!loading && 
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          {users.map((user) => (
+            <ListItem key={user.id}>
+              <Link to={`/users/${user.id}`}>{user.name}</Link>
+            </ListItem>
+          ))}
+        </List>
+      }
     </div>
   );
 }
